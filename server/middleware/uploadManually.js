c4c6222025c25f5405a63789faa9ac9fb8126302
KostasBzn <kostasbouzianis@gmail.com>
2024-03-12 13:12:58 +0100

    fixed cloudinary env bug

---
+++ server/middleware/uploadManually.js
@@ -0,0 +1,30 @@
+import cloudinaryV2 from "../config/cloudinary.js";
+
+cloudinaryV2.uploader.upload(
+  //The exact path in hard drive
+  "C:/Users/Kostas/Desktop/IMG_20240110_112217.jpg",
+
+  {
+    folder: "Simona_Final_Project/backgrounds",
+    public_id: "Name_of_the_image", //A name for the image
+  },
+  function (error, result) {
+    if (error) {
+      console.error("Error uploading the image:", error);
+    } else {
+      console.log("Image uploaded successfully:", result);
+    }
+  }
+);
+
+/* This small function is created to upload our page backrounds to the cloudinary and use them as url, to avoid using external surces that in the future they may not work. */
+
+//Instructions:
+
+//1. Paste the exact path of the image on your computer
+//2. Including the extension(.jpg, .png ect)
+//3. give a name for the image in the public_id string
+//4. run the file in the terminal. type: node uploadManually.js
+//5. copy the url path that will be printet in the tertminal to use the image.
+
+//Done!!!
