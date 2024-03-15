be4b924f5d89a55e477e02e4ee963965112fd69a
KostasBzn <kostasbouzianis@gmail.com>
2024-03-15 10:58:05 +0100

    alert components  added

--- client/src/pages/RegisterUserCategoryForm.jsx
+++ client/src/pages/RegisterUserCategoryForm.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import FormSubmitButton from "../components/FormSubmitButton";
+import AlertMessageWarning from "../components/AlertMessageWarning";
 
 const RegisterUserCategoryForm = () => {
   const [selectedCategories, setSelectedCategories] = useState([]);
@@ -96,28 +97,7 @@ const RegisterUserCategoryForm = () => {
           <FormSubmitButton name="Next" />
         </form>
         {showLimitChoiseMessage && (
-          <div class="bg-yellow-50 border border-yellow-400 rounded text-yellow-800 text-sm p-4 flex fixed  items-start  top-0  left-1/2 transform -translate-x-1/2">
-            <div>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="h-4 w-4 mr-2"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
-                  clip-rule="evenodd"
-                />
-              </svg>
-            </div>
-            <div class="w-full">
-              <p>
-                <span class="font-bold">Info: </span>
-                You've reached the category selection limit.
-              </p>
-            </div>
-          </div>
+          <AlertMessageWarning text="You've reached the chice limit!" />
         )}
       </div>
     </>
