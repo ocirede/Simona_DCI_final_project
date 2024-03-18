74a0a1cbfede129696a572cbbb376ed2162c2ffb
KostasBzn <kostasbouzianis@gmail.com>
2024-03-18 11:20:23 +0100

    merged and working

--- client/src/layouts/ProfilePageLayout.jsx
+++ client/src/layouts/ProfilePageLayout.jsx
@@ -1,12 +1,14 @@
+import { Outlet } from "react-router-dom";
+import NavBarHomepage from "../components/navBarHomepage";
 import ProfilePageEntrepreneur from "../pages/ProfilePageEntrepreneur";
-import { Footer } from "./components/Footer";
+import Footer from "./components/Footer";
 
 export default function ProfilePageLayout() {
-    return (
-        <div>
-            <ProfilePageEntrepreneur />
-            <Footer/>
-        </div>
-    );
+  return (
+    <div>
+      <NavBarHomepage />
+      <Outlet />
+      <Footer />
+    </div>
+  );
 }
-
