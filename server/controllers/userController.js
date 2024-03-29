6f367617e2a1df422fe32030b332972fc90daa40
Tyhe Ferenc <tyhe444@gmail.com>
2024-03-29 15:08:40 +0100

    minor fix

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -114,44 +114,7 @@ export const updatePassword = async (req, res) => {
   }
 };
 
-// fetching artists
-// //get Artists
-// export const getArtists = async (req, res) => {
-//   const { role } = req.query;
 
-//   try {
-//     let filter = {};
-//     if (role) {
-//       filter.role = { $regex: role, $options: "i" };
-//     }
-
-//     const artists = await User.find(filter);
-
-//     res.send({ success: true, artists });
-//   } catch (error) {
-//     console.error("Error fetching the artists", error.message);
-//     res.send({ succsess: false, error: error.message });
-//   }
-// };
-
-// // fetching entrepeneurs
-// export const getEntrepreneurs = async (req, res) => {
-//   const { role } = req.query;
-
-//   try {
-//     let filter = {};
-//     if (role) {
-//       filter.role = { $regex: role, $options: "i" };
-//     }
-
-//     const entrepreneurs = await User.find(filter);
-
-//     res.send({ success: true, entrepreneurs });
-//   } catch (error) {
-//     console.error("Error fetching the Entrepreneurs", error.message);
-//     res.send({ succsess: false, error: error.message });
-//   }
-// };
 
 //Send connect request
 export const sendConnectionRequest = async (req, res) => {
