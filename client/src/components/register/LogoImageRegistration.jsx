02f5a12be41ed02de336b0dbf84e35584fae4bd8
KostasBzn <kostasbouzianis@gmail.com>
2024-04-02 22:09:08 +0200

    register process styled

--- client/src/components/register/LogoImageRegistration.jsx
+++ client/src/components/register/LogoImageRegistration.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const LogoImageRegistration = ({ imageUrl }) => {
-  return <h2 className="absolute top-0 left-0 mt-4 ml-4 h-16">Simona</h2>;
+  return (
+    <h2 className="absolute top-0 left-0 mt-4 ml-4 h-16 simona text-[30px] sm:text-[40px]">
+      Simona
+    </h2>
+  );
 };
 
 export default LogoImageRegistration;
