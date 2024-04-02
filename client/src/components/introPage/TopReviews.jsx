08778bf5754ef4e4ada6ff4f1b343af12dd81e00
KostasBzn <kostasbouzianis@gmail.com>
2024-04-02 18:14:11 +0200

    best rating comments intro page fixed after merge

--- client/src/components/introPage/TopReviews.jsx
+++ client/src/components/introPage/TopReviews.jsx
@@ -1,13 +1,18 @@
+import { useContext, useEffect } from "react";
 import fullStarSvg from "../../assets/rating_svg/star.png";
+import { RatingContext } from "../../context/ratingContext";
 
-export default function TopReviews({ reviews }) {
-
+export default function TopReviews() {
+  const { bestRatedComments, getBestRatedComments } = useContext(RatingContext);
+  useEffect(() => {
+    getBestRatedComments();
+  }, []);
   return (
     <div className="bg-cobaltBlue pb-4 rounded-2xl">
       <h2 className="uppercase text-[40px] mb-5 mt-10 pt-4 text-center">
         Top Reviews
       </h2>
-      {reviews?.map((review, index) => (
+      {bestRatedComments?.map((review, index) => (
         <div key={index} className="m-5 flex flex-col items-center text-center">
           <div className="flex items-center">
             {[...Array(review.ratingNumber)].map((_, index) => (
@@ -20,7 +25,6 @@ export default function TopReviews({ reviews }) {
             ))}
           </div>
           <p className="mt-2 text-white">{review.comment}</p>
-
         </div>
       ))}
     </div>
